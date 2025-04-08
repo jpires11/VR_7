8e2916f3496b59a2cf686a73ea0ad7116117d5f3
fengxunling <wenxin.che@epfl.ch>
2025-04-08 21:46:34 +0200

    update

--- Assets/Rooms/scripts/socket_2.cs
+++ Assets/Rooms/scripts/socket_2.cs
@@ -1,20 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
 
 public class CustomSocketInteraction2 : MonoBehaviour
 {
     public string validTag = "Letter_A";
+    public bool isSocketed = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(validTag))
         {
             Debug.Log("Valid object detected!");
+            isSocketed = true;
         }
         else
         {
             other.gameObject.SetActive(false);
             Debug.Log("Invalid object detected and disabled!");
+            StartCoroutine(ReactivateObject(other.gameObject, 1f));
         }
+    }
+
+    private IEnumerator ReactivateObject(GameObject obj, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        obj.SetActive(true);
+        obj.transform.position += new Vector3(0, -0.4f, 0);
     }
 }
