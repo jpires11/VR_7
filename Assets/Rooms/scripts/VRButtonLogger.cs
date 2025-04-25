d03ff0494dbe45669c0ff20052159506ede3f295
fengxunling <wenxin.che@epfl.ch>
2025-04-25 15:33:26 +0200

    tmp

---
+++ Assets/Rooms/scripts/VRButtonLogger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class VRButtonLogger : MonoBehaviour
+{
+    private InputDevice rightController;
+
+    void Start()
+    {
+        // 获取右手控制器
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, devices);
+        if (devices.Count > 0)
+        {
+            rightController = devices[0];
+        }
+    }
+
+    void Update()
+    {
+        if (rightController.isValid)
+        {
+            bool primaryButtonValue = false;
+            if (rightController.TryGetFeatureValue(CommonUsages.primaryButton, out primaryButtonValue) && primaryButtonValue)
+            {
+                Debug.Log("VR 控制器按钮被点击！");
+            }
+        }
+    }
+
+    public void OnButtonClick()
+    {
+        Debug.Log("UI Button 被点击！");
+    }
+}
