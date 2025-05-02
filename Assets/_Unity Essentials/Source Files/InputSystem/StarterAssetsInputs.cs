5b5a8b5c3b6937a9de96a8b2b6e04dd899bb3fe2
HanqiLu <hanqi.lu@epfl.ch>
2025-05-02 16:43:57 +0200

    sync room

--- Assets/_Unity Essentials/Source Files/InputSystem/StarterAssetsInputs.cs
+++ Assets/_Unity Essentials/Source Files/InputSystem/StarterAssetsInputs.cs
@@ -7,7 +7,11 @@ namespace StarterAssets
 {
 	public class StarterAssetsInputs : MonoBehaviour
 	{
-		[Header("Character Input Values")]
+        public InputActionProperty moveAction;
+        public InputActionProperty jumpAction;
+        public InputActionProperty sprintAction;
+
+        [Header("Character Input Values")]
 		public Vector2 move;
 		public Vector2 look;
 		public bool jump;
@@ -19,6 +23,13 @@ namespace StarterAssets
 		[Header("Mouse Cursor Settings")]
 		public bool cursorLocked = true;
 		public bool cursorInputForLook = true;
+
+        private void Start()
+        {
+            moveAction.action.performed += x => MoveInput(x.ReadValue<Vector2>());
+            jumpAction.action.performed += x => JumpInput(x.action.IsPressed());
+            sprintAction.action.performed += x => SprintInput(x.action.IsPressed());
+        }
 
 #if ENABLE_INPUT_SYSTEM
 		public void OnMove(InputValue value)
@@ -46,7 +57,7 @@ namespace StarterAssets
 #endif
 
 
-		public void MoveInput(Vector2 newMoveDirection)
+        public void MoveInput(Vector2 newMoveDirection)
 		{
 			move = newMoveDirection;
 		} 
